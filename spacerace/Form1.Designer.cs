b0e4b1086ed20d7235056176b863e6b90f2c4bf1
mattbean292 <mattbean292@SDS-L-231HC63>
2024-05-23 13:46:07 -0400

    Add project files.

---
+++ spacerace/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace spacerace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.player1scoreLabel = new System.Windows.Forms.Label();
+            this.player2scoreLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Label();
+            this.AILabel = new System.Windows.Forms.Label();
+            this.tellplayerLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // player1scoreLabel
+            // 
+            this.player1scoreLabel.AutoSize = true;
+            this.player1scoreLabel.Location = new System.Drawing.Point(174, 9);
+            this.player1scoreLabel.Name = "player1scoreLabel";
+            this.player1scoreLabel.Size = new System.Drawing.Size(35, 13);
+            this.player1scoreLabel.TabIndex = 0;
+            this.player1scoreLabel.Text = "label1";
+            // 
+            // player2scoreLabel
+            // 
+            this.player2scoreLabel.AutoSize = true;
+            this.player2scoreLabel.Location = new System.Drawing.Point(396, 9);
+            this.player2scoreLabel.Name = "player2scoreLabel";
+            this.player2scoreLabel.Size = new System.Drawing.Size(35, 13);
+            this.player2scoreLabel.TabIndex = 1;
+            this.player2scoreLabel.Text = "label1";
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(212, 163);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(175, 34);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "Start";
+            this.startButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click_1);
+            // 
+            // AILabel
+            // 
+            this.AILabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AILabel.Location = new System.Drawing.Point(250, 283);
+            this.AILabel.Name = "AILabel";
+            this.AILabel.Size = new System.Drawing.Size(100, 34);
+            this.AILabel.TabIndex = 4;
+            this.AILabel.Text = "Vs AI";
+            this.AILabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.AILabel.Click += new System.EventHandler(this.AILabel_Click);
+            // 
+            // tellplayerLabel
+            // 
+            this.tellplayerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tellplayerLabel.Location = new System.Drawing.Point(81, 22);
+            this.tellplayerLabel.Name = "tellplayerLabel";
+            this.tellplayerLabel.Size = new System.Drawing.Size(434, 34);
+            this.tellplayerLabel.TabIndex = 5;
+            this.tellplayerLabel.Text = "Space Race";
+            this.tellplayerLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(600, 600);
+            this.Controls.Add(this.tellplayerLabel);
+            this.Controls.Add(this.AILabel);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.player2scoreLabel);
+            this.Controls.Add(this.player1scoreLabel);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label player1scoreLabel;
+        private System.Windows.Forms.Label player2scoreLabel;
+        private System.Windows.Forms.Label startButton;
+        private System.Windows.Forms.Label AILabel;
+        private System.Windows.Forms.Label tellplayerLabel;
+    }
+}
+
---
+++ spacerace/Form1.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace spacerace
+{
+    public partial class Form1 : Form
+    {
+        List<Rectangle> asteroidList = new List<Rectangle>();
+        List<int> asteroidDirection = new List<int>();
+        List<int> asteroidSpeed = new List<int>();      
+        int asteroidspot;
+
+        Rectangle player1 = new Rectangle(120, 570, 10, -30);
+        Rectangle player2 = new Rectangle(420, 570, 10, -30);
+        Rectangle firePlayer1 = new Rectangle(120, 615, 10, -30);
+        Rectangle firePlayer2 = new Rectangle(420, 615, 10, -30);
+        Rectangle touchbox = new Rectangle(387, 540, 75, 50);
+
+        Rectangle timelimit = new Rectangle(300, 600, 10, -600);
+
+
+        int playerspeed = 3;
+        int player1point;
+        int player2point;
+
+        bool wPressed = false;
+        bool sPressed = false;
+        bool upPressed = false;
+        bool downPressed = false;
+        bool qPressed = false;
+        bool AI = false;
+
+        SoundPlayer victory = new SoundPlayer(Properties.Resources.victory);
+        SoundPlayer crickets = new SoundPlayer(Properties.Resources.crickets);
+
+        int x;
+        int timer;
+
+        Random randGen = new Random();
+        int randValue;
+
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush orangeBrush = new SolidBrush(Color.Orange);
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
+        public Form1()
+        {
+            InitializeComponent();            
+            startButton.BackColor = Color.Black;
+            player1scoreLabel.Text = "";
+            player2scoreLabel.Text = "";
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+
+                case Keys.W:
+                    wPressed = true;
+                    break;
+                case Keys.S:
+                    sPressed = true;
+                    break;
+                case Keys.Up:
+                    upPressed = true;
+                    break;
+                case Keys.Down:
+                    downPressed = true;
+                    break;
+                case Keys.Q:
+                    qPressed = true;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+
+                case Keys.W:
+                    wPressed = false;
+                    break;
+                case Keys.S:
+                    sPressed = false;
+                    break;
+                case Keys.Up:
+                    upPressed = false;
+                    break;
+                case Keys.Down:
+                    downPressed = false;
+                    break;
+                case Keys.Q:
+                    qPressed = false;
+                    break;
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (gameTimer.Enabled == false && timer > 0)
+            {
+                asteroidList.Clear();
+                tellplayerLabel.Visible = true;
+                startButton.Enabled = true;
+                startButton.Visible = true;
+                AILabel.Enabled = true;
+                AILabel.Visible = true;
+                startButton.Text = "Try Again";
+                AILabel.Text = "Bye";
+                if (player1point > player2point)
+                {
+                    tellplayerLabel.Text = "Player1 Wins";
+                    victory.Play();
+                }
+                else if (player1point < player2point)
+                {
+                    tellplayerLabel.Text = "Player2 Wins";
+                    victory.Play();
+                }
+                else
+                {
+                    tellplayerLabel.Text = "You both suck";
+                    crickets.Play();
+                }
+            }
+            else
+            {
+                if (wPressed == true)
+                {
+                    e.Graphics.FillEllipse(orangeBrush, firePlayer1);
+                }
+                if (upPressed == true && AI == false)
+                {
+                    e.Graphics.FillEllipse(orangeBrush, firePlayer2);
+                }
+                if (AI == true)
+                {
+                    e.Graphics.FillEllipse(orangeBrush, firePlayer2);
+                    for (int i = 0; i < asteroidList.Count; i++)
+                    {
+                        if (asteroidList[i].IntersectsWith(touchbox))
+                        {
+                            e.Graphics.FillEllipse(blackBrush, firePlayer2);
+                        }
+                    }
+                }
+                e.Graphics.FillRectangle(whiteBrush, player1);
+                e.Graphics.FillRectangle(whiteBrush, player2);              
+                e.Graphics.FillRectangle(whiteBrush, timelimit);
+                if (qPressed == true)
+                {
+                    e.Graphics.FillRectangle(whiteBrush, touchbox);
+                }
+
+                for (int i = 0; i < asteroidList.Count; i++)
+                {
+                    e.Graphics.FillRectangle(whiteBrush, asteroidList[i]);
+                }
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {                                              
+            //player1 movement
+            if (wPressed == true)
+            {
+               
+                    player1.Y = player1.Y - playerspeed;
+                firePlayer1.Y = firePlayer1.Y - playerspeed;
+            }
+            
+                if (sPressed == true && player1.Y < this.Height - player1.Height)
+                {
+                   
+                        player1.Y = player1.Y + playerspeed;
+                firePlayer1.Y = firePlayer1.Y + playerspeed;
+            }
+
+            //player2 movement
+            if (AI == true) 
+            {
+                
+
+                for (int i = 0; i < asteroidList.Count; i++) {
+                    if (asteroidList[i].IntersectsWith(touchbox) && player2.Y < this.Height - player2.Height)
+                        {
+                        touchbox.Y = touchbox.Y + playerspeed;
+                        player2.Y = player2.Y + playerspeed;
+                        firePlayer2.Y = firePlayer2.Y + playerspeed;
+                    }                   
+                }
+                
+                touchbox.Y = touchbox.Y - playerspeed;
+                player2.Y = player2.Y - playerspeed;
+                firePlayer2.Y = firePlayer2.Y - playerspeed;
+            }
+            else
+            {
+                if (upPressed == true)
+                {
+
+                    player2.Y = player2.Y - playerspeed;
+                    firePlayer2.Y = firePlayer2.Y - playerspeed;
+                }
+
+                if (downPressed == true && player2.Y < this.Height - player2.Height)
+                {
+
+                    player2.Y = player2.Y + playerspeed;
+                    firePlayer2.Y = firePlayer2.Y + playerspeed;
+                }
+            }
+
+            //points
+            
+                if (player1.Y < 10)
+                {
+
+                player1.Y = this.Height - player1.Height;
+                firePlayer1.Y = this.Height + 15;
+                player1point++;
+                    
+                }
+            
+            
+                if (player2.Y < 10)
+                {
+
+                player2.Y = this.Height - player1.Height;
+                firePlayer2.Y = this.Height + 15;
+                touchbox.Y = this.Height - 60;
+                player2point++;
+                    
+                }
+            
+            //projectiles
+            if (asteroidList.Count < 12)
+            {
+                randValue = randGen.Next(1,3);
+                if (randValue == 1)
+                {
+                    asteroidspot = 0;
+                    asteroidDirection.Add(asteroidspot);                    
+                }
+                else
+                {
+                    asteroidspot = this.Width;
+                    asteroidDirection.Add(asteroidspot);
+                }
+                asteroidSpeed.Add(randGen.Next(5, 10));
+                randValue = randGen.Next(10, this.Height - 5);
+                Rectangle asteroid = new Rectangle(asteroidspot, randValue, 10, 5);
+                asteroidList.Add(asteroid);          
+            }
+            for (int i = 0; i < asteroidList.Count; i++)
+            {
+                if (asteroidList[i].X >= this.Width + asteroidList[i].Width || asteroidList[i].X <= 0 - asteroidList[i].Width)
+                {
+                    asteroidList.RemoveAt(i);
+                    asteroidDirection.RemoveAt(i);
+                    asteroidSpeed.RemoveAt(i);
+                }
+            }
+            // projectile movement
+            for (int i = 0; i < asteroidList.Count; i++)
+            {
+                if (asteroidDirection[i] == 0)
+                {
+                    x = asteroidList[i].X + asteroidSpeed[i];
+                    asteroidList[i] = new Rectangle(x, asteroidList[i].Y, 10, 5);
+                }
+                else
+                {
+                    x = asteroidList[i].X - asteroidSpeed[i];
+                    asteroidList[i] = new Rectangle(x, asteroidList[i].Y, 10, 5);
+                }
+            }
+            // projectile interaction
+            for (int i = 0; i < asteroidList.Count; i++)
+            {
+                
+                    if (asteroidList[i].IntersectsWith(player1))
+                    {
+
+                    player1.Y = this.Height - player1.Height;
+                    firePlayer1.Y = this.Height + 15;
+
+                }
+             
+               
+                    if (asteroidList[i].IntersectsWith(player2))
+                    {
+                       
+                    player2.Y = this.Height - player1.Height;
+                    firePlayer2.Y = this.Height + 15;
+                    touchbox.Y = this.Height - 60;
+                }
+                
+            }
+            // time limit
+             timelimit = new Rectangle(300, 0 + timer, 10, 600 - timer);
+            if (timer == 600)
+            {
+                gameTimer.Stop();
+            }
+             
+            player1scoreLabel.Text = $"{player1point}";
+            player2scoreLabel.Text = $"{player2point}";
+            timer++;
+            Refresh();
+        }
+
+        private void startButton_Click_1(object sender, EventArgs e)
+        {
+            if (gameTimer.Enabled == false && timer > 0)
+            {
+                crickets.Stop();
+                victory.Stop();
+                tellplayerLabel.Text = "Space Race";
+                startButton.Text = "Start";
+                AILabel.Text = "Vs AI";
+                player1scoreLabel.Text = "";
+                player2scoreLabel.Text = "";
+                player1point = 0;
+                player2point = 0;
+                timer = 0;
+            }
+            else
+            {
+                player1 = new Rectangle(120, 570, 10, 30);
+                player2 = new Rectangle(420, 570, 10, 30);
+                firePlayer1 = new Rectangle(120, 615, 10, -30);
+                firePlayer2 = new Rectangle(420, 615, 10, -30);
+                startButton.Enabled = false;
+                startButton.Visible = false;
+                AILabel.Enabled = false;
+                AILabel.Visible = false;
+                tellplayerLabel.Visible = false;
+                gameTimer.Start();
+            }
+        }
+
+        private void AILabel_Click(object sender, EventArgs e)
+        {
+            if (gameTimer.Enabled == false && timer > 0)
+            {
+             Application.Exit();
+            }
+
+            player1 = new Rectangle(120, 570, 10, 30);
+            player2 = new Rectangle(420, 570, 10, 30);
+            firePlayer1 = new Rectangle(120, 615, 10, -30);
+            firePlayer2 = new Rectangle(420, 615, 10, -30);
+            startButton.Enabled = false;
+            startButton.Visible = false;
+            AILabel.Enabled = false;
+            AILabel.Visible = false;
+            tellplayerLabel.Visible = false;
+            AI = true;
+            gameTimer.Start();
+        }
+    }
+}
